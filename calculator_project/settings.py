a7923ebf889f7feb16336a266ddec77dbce48b96
Varshini Koppisetti <varshinikoppisetti999@gmail.com>
2026-01-13 13:17:42 +0530

    added

--- calculator_project/settings.py
+++ calculator_project/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-temp-key'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
